27f43ec0b4dd47a49276a7cbd21169781a3f2f93
Jorge Chavez <53054594+jorgearmandooc@users.noreply.github.com>
2023-09-16 18:50:57 -0600

    fix: bienvenida path

--- src/main/java/com/gapsi/gapsiecommerce/controller/BienvenidaController.java
+++ src/main/java/com/gapsi/gapsiecommerce/controller/BienvenidaController.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("bievenida")
+@RequestMapping("bienvenida")
 public class BienvenidaController {
 
     @GetMapping
